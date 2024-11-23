0b69f9363c90ebb135bf32ae81b0f68b2df5b577
VisualZac <144742015+VisualZac@users.noreply.github.com>
2024-11-23 07:51:32 -0300

    Create pago.php

---
+++ pago.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<script src="https://www.paypal.com/sdk/js?components=buttons,card-fields&client-id=<%= 
+Ad3RN_lEr-8p0p-XmDaXmSB3StC1U4o3YoGdhyOLumH-L1tv_Z45ysILbv9__osdKFJ7sYqNODycKxtQ&currency=UY%>"></script>
+<body>
+
+    <div id="paypal-button-conteiner"></div>    
+
+</body>
+</html>
